08a0c5fe64dd2091ae6d7f0de16b38e559e1c9f2
Josh C <cowley.josh@gmail.com>
2020-05-10 21:41:31 -0400

    Intro to python MVP

--- src/03_modules.py
+++ src/03_modules.py
@@ -9,23 +9,24 @@ import sys
 # See docs for the sys module: https://docs.python.org/3.7/library/sys.html
 
 # Print out the command line arguments in sys.argv, one per line:
-# YOUR CODE HERE
+print(sys.argv)
 
 # Print out the OS platform you're using:
-# YOUR CODE HERE
+print("OS Platform:" + sys.platform)
 
 # Print out the version of Python you're using:
-# YOUR CODE HERE
+print("Python Version:" + sys.version)
 
 
 import os
 # See the docs for the OS module: https://docs.python.org/3.7/library/os.html
 
 # Print the current process ID
-# YOUR CODE HERE
+pid = os.getpid()
+print("Current Process ID:"+ str(pid))
 
 # Print the current working directory (cwd):
-# YOUR CODE HERE
+print("Current Working Directory:" + os.getcwd())
 
 # Print out your machine's login name
-# YOUR CODE HERE
+print("Login Name:" + os.getlogin())
--- src/09_dictionaries.py
+++ src/09_dictionaries.py
@@ -34,14 +34,26 @@ waypoints = [
 ]
 
 # Add a new waypoint to the list
-# YOUR CODE HERE
+waypoints.append(
+    {
+        "lat": 66,
+        "lon": 28,
+        "name": "Somewhere over the Rainbow"
+    }
+)
 
 # Modify the dictionary with name "a place" such that its longitude
 # value is -130 and change its name to "not a real place"
 # Note: It's okay to access the dictionary using bracket notation on the
 # waypoints list.
 
-# YOUR CODE HERE
+waypoints[0] = {
+    "lat": 43,
+    "lon": -130,
+    "name": "not a real place"
+}
 
 # Write a loop that prints out all the field values for all the waypoints
-# YOUR CODE HERE
+for wp in waypoints:
+    print(wp["lat"],wp["lon"], wp["name"])
+    
